d67db11cb2d656c27dcb90081c5896b33ca2177f
天然 <49628628+tianran721@users.noreply.github.com>
2022-06-10 14:05:27 +0800

    大文件上传

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ function copy(src, dst) {
 	fs.writeFileSync(dst, file);
 	console.log("copy成功");
 }
-
+function copyBigFile(src, dst) {
+	fs.createReadStream(src).pipe(fs.createWriteStream(dst));
+}
 function main(argv) {
-	copy(argv[0], argv[1]);
+	// copy(argv[0], argv[1]);
+	copyBigFile(argv[0], argv[1]);
 }
 main(process.argv.slice(2));
